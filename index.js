d89f261763370ba554ce7d6398a747d5a8f9dcff
Colin Brence <cbrence@ramapo.edu>
2018-04-12 23:02:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,12 @@ function logWhisper(string) {
  return(string.toLowerCase())
 }
 
-function sayHiToGrandma() {
-  var lowercase = "HELLO!"
-  uppercase.toUppercase()===uppercase
+funtion sayHiToGrandma(string){ 
+  if(string.toLowerCase===string) 
+  console.log('I can\'t hear you!')
   
- var lowercase = 'hello!'
- lowercase.toLowerCase() === lowercase // true
- 
-var mixedCase = 'Hi there!'
- mixedCase.toLowerCase() === mixedCase // false
- mixedCase.toUpperCase() === mixedCase 
+  if (string.toUpperCase===string) 
+  console.log('YES INDEED!')
+  
+  if(string==="I love you, Grandma.") 
+  console.log('I love you, too.') }
